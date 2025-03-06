9acea822a6a04499b1d0cb58e017f604e3a5fd24
AsH1605 <akshitatiwary16@gmail.com>
2025-03-06 23:11:43 +0530

    Project completed

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,5 +1,5 @@
 import bodyParser from "body-parser";
-import { Router } from "express";555555555555555555
+import { Router } from "express";
 import { loginUser, registerUser } from "../controller/user.controller";
 
 export const userRouter = Router()
